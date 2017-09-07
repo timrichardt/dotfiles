5fb77519b7cf4d5f4dec9742203ff70c1067cf90
Tim Richardt <tim@snafu.cc>
2017-09-07 16:23:31 +0200

    Add `~/bin`, correct basic stumpwm

--- stumpwm/.stumpwm.d/init.lisp
+++ stumpwm/.stumpwm.d/init.lisp
@@ -2,18 +2,12 @@
 ;;
 ;; Here is a sample .stumpwmrc file
 
-(require :swank)
-
-(require :stumpwm)
-
 (in-package :stumpwm)
+
+(require :swank)
 
 (swank-loader:init)
 
-(swank:create-server :port 5556
+(swank:create-server :port 4004
                      :style swank:*communication-style*
                      :dont-close t)
-
-(stumpwm:stumpwm)
-
-(set-prefix-key (kbd "C-t"))
